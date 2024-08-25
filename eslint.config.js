937cc879a9d703b15fca9f0509a100f6c88884f8
Jay <jay.sanjay.karia@gmail.com>
2024-08-25 20:14:47 +0530

    chore: initial project setup

---
+++ eslint.config.js
@@ -0,0 +1,10 @@
+export default [
+  {
+    rules: {
+      eqeqeq: "off",
+      "no-unused-vars": "error",
+      "prefer-const": ["error", { ignoreReadBeforeAssign: true }],
+      camelcase: "error",
+    },
+  },
+];
